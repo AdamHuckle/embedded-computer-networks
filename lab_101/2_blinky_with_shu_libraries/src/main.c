1a493bf49d5e356d861c03d58bd6505b2fbcff7b
AdamHuckle <43818259+AdamHuckle@users.noreply.github.com>
2018-10-03 16:50:28 +0100

    end of lab 1- end of traffic light

--- lab_101/2_blinky_with_shu_libraries/src/main.c
+++ lab_101/2_blinky_with_shu_libraries/src/main.c
@@ -19,8 +19,9 @@
 #include "gpio.h"
 
 // map the led to GPIO PI1 (again, this is the inbuilt led)
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
-
+gpio_pin_t Red = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t Yellow = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t Green = {PB_15, GPIOB, GPIO_PIN_15};
 // this is the main method
 int main()
 {
@@ -30,15 +31,53 @@ int main()
   init_sysclk_216MHz();
   
   // initialise the gpio pins
-  init_gpio(led, OUTPUT);
-  
+  init_gpio(Red, OUTPUT);
+  init_gpio(Yellow, OUTPUT);
+	init_gpio(Green, OUTPUT);
   // loop forever ...
   while(1)
   {
-    // toggle the led on the gpio pin
-    toggle_gpio(led);
     
-    // wait for 1 second
-    HAL_Delay(1000);
+	// Stage 1 Red Only 
+    write_gpio (Red, HIGH);		// turn the red led on 
+		write_gpio (Yellow, LOW);		// turn the Yellow led off
+		write_gpio (Green, LOW);		// turn the Green led off
+		
+  // wait for 1 second
+    HAL_Delay(1500);		// delay for 1.5 second 
+		
+	// Stage 2 Red and Yellow 
+    write_gpio (Red, HIGH);		// turn the red led on 
+		write_gpio (Yellow, HIGH);		// turn the Yellow led on
+		write_gpio (Green, LOW);		// turn the Green led off
+	
+	// wait for 1 second
+    HAL_Delay(500);		// delay for 1.5 second 
+		
+	// Stage 3 Yellow Only 
+		write_gpio (Red, LOW);		// turn the red led off 
+		write_gpio (Yellow, HIGH);		// turn the Yellow led on
+		write_gpio (Green, LOW);		// turn the Green led off
+
+	// wait for 1 second
+    HAL_Delay(1000);		// delay for 1.5 second		
+		
+	// Stage 4 Green Only
+		
+		write_gpio (Red, LOW);		// turn the red led off 
+		write_gpio (Yellow, LOW);		// turn the Yellow led off
+		write_gpio (Green, HIGH);		// turn the Green led on
+		
+	// wait for 1 second
+    HAL_Delay(1500);		// delay for 1.5 second		
+		
+	//Stage 5	Yellow Only 
+	
+		write_gpio (Red, LOW);		// turn the red led off 
+		write_gpio (Yellow, HIGH);		// turn the Yellow led on
+		write_gpio (Green, LOW);		// turn the Green led off
+
+// wait for 1 second
+    HAL_Delay(1000);		// delay for 1.5 second		
   }
 }
